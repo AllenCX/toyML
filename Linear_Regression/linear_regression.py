8b738192fd5226f30601c28213fa06e5679fdc54
AllenCX <allen.caixy@gmail.com>
2016-10-22 02:31:23 +0000

    added linear regression

--- Linear_Regression/linear_regression.py
+++ Linear_Regression/linear_regression.py
@@ -1,14 +1,31 @@
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.linalg import inv
-x = np.linspace(0, 10, num=51).reshape(51, 1)
-y = np.linspace(0, 10, num=51).reshape(51, 1)
-t = np.random.normal(0, 2, (51, 1))
+#%matplotlib inline
+N = 50
+
+#simple 2D linear regression example
+x = np.linspace(0, 10, num=N).reshape(N, 1)
+y = np.linspace(0, 10, num=N).reshape(N, 1)
+t = np.random.normal(0, 1, (N, 1))
 y += t
 beta_hat = inv(x.T.dot(x)).dot(x.T).dot(y)
 
 fig, ax = plt.subplots()
-ax.scatter(x, y, 'b')
-ax.plot(x, beta_hat*x, 'r')
+ax.plot(x, y, 'bo')
+ax.plot(x, x.dot(beta_hat), 'r')
 plt.show()
 
+#simple 3D linear regressison example
+z = np.linspace(10, 20, num=N).reshape(N, 1)
+x_3d = np.concatenate((x, z), axis=1)
+
+beta_hat_3d = inv(x_3d.T.dot(x_3d)).dot(x_3d.T).dot(y)
+
+fig_3d = plt.figure()
+ax_3d = fig_3d.add_subplot(111, projection='3d')
+ax_3d.scatter(x, z, y)
+y_3d_hat = x_3d.dot(beta_hat_3d)
+ax_3d.plot(x, z, y_3d_hat.reshape(N), color='red')
+plt.show()
